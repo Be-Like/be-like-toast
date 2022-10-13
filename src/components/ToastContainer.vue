3a56d69568cbc757b53b1c58309e980760844558
Be-Like <simpson.jacob.barry@gmail.com>
2022-10-13 10:14:05 -0400

    Basic container and card

---
+++ src/components/ToastContainer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="toast-container">
+    <toast-card v-for="i in 5" :key="i" />
+  </div>
+</template>
+
+<script>
+import ToastCard from "./ToastCard.vue";
+
+export default {
+  components: { ToastCard },
+};
+</script>
+
+<style lang="scss" scoped>
+#toast-container {
+  bottom: 0;
+  left: 0;
+  padding: 10px;
+  position: fixed;
+  width: fit-content;
+  z-index: 50;
+
+  & > * {
+    margin: 10px 0;
+  }
+}
+</style>
